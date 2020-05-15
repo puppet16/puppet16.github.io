fdbddd066b517057aec0b60d4689f769625f874f
litongtong5 <litongtong5@xdf.cn>
2020-05-15 14:58:56 +0800

    添加宠物

--- themes/anatole-core/source/js/baidu-tongji.js
+++ themes/anatole-core/source/js/baidu-tongji.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?";
+  hm.src = "https://hm.baidu.com/hm.js?https://hm.baidu.com/hm.js?befa08ce984072f8cbfbe3161cdc5d5d";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
